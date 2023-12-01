e5e2393dbc7ad59ebecdb1d9956aba478495c420
Ken <kenoh17@uw.edu>
2023-12-01 01:56:30 -0800

    added link to data

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -3,6 +3,7 @@ import geopandas as gpd
 
 def clean_restaurant_data():
     # I had to delete this file after filtering it because it was too big for GitHub
+    # The file can be found here: https://gis-kingcounty.opendata.arcgis.com/datasets/kingcounty::restaurant-inspections-restaurant-inspections-point/explore?location=47.449024%2C-121.952400%2C10.87
     file_path = './assets/Restaurant_Inspections___restaurant_inspections_point.geojson'
     df = gpd.read_file(file_path)
     df = df[(df['CITY'] == 'Seattle') &

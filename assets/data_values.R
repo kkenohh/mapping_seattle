12940e118d3d30f6c679a75f48486b87fb380799
Lesther <lestherdumosmog@gmail.com>
2023-12-05 15:31:15 -0800

    Used R to find max values within assets data

---
+++ assets/data_values.R
@@ -0,0 +1,21 @@
+library(sf)
+
+# Create data table from assets
+geojson_file <- "Cleaned_Restaurant_Inspections.geojson"
+sf_data <- st_read(geojson_file)
+str(sf_data)
+
+#Max Score Inspection, 98
+max_score_insp <- max(sf_data$SCORE_INSPECTION, na.rm = TRUE)
+
+#Max Violation Points, 25
+sf_data$VIOLATIONPOINTS <- as.numeric(sf_data$VIOLATIONPOINTS)
+max_violation_points <- max(sf_data$VIOLATIONPOINTS, na.rm = TRUE)
+
+#Amount of Risks
+risk <- table(sf_data$RISK)
+count_rI <- risk_I["I"]         #608
+count_rII <- risk_I["II"]       #887
+count_rIII <- risk_I["III"]     #7081
+
+

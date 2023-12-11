8c0ff021764e8954b069495e0d76e1546bb31185
Ken <kenoh17@uw.edu>
2023-12-10 23:55:51 -0800

    did sort by distance

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -1,11 +1,11 @@
 import geopandas as gpd
 
 
+# Does the initial cleaning
 def clean_restaurant_data():
     # I had to delete this file after filtering it because it was too big for GitHub
     # The file can be found here: https://gis-kingcounty.opendata.arcgis.com/datasets/kingcounty::restaurant-inspections-restaurant-inspections-point/explore?location=47.449024%2C-121.952400%2C10.87
-    file_path = './assets/Restaurant_Inspections__'
-    + '_restaurant_inspections_point.geojson'
+    file_path = './assets/Restaurant_Inspections___restaurant_inspections_point.geojson'
     df = gpd.read_file(file_path)
     df = df[(df['CITY'] == 'Seattle') &
             (df['DATE_INSPECTION'].astype(str).str[:4] == '2023')]
@@ -15,8 +15,20 @@ def clean_restaurant_data():
                driver='GeoJSON')
 
 
+# Gets rid of duplicated restaurants
+def get_rid_duplicates():
+    file_path = './assets/Cleaned_Restaurant_Inspections.geojson'
+    df = gpd.read_file(file_path)
+    duplicate_mask = df['NAME'].duplicated(keep='first')
+    df = df[~duplicate_mask]
+    df.to_file('./assets/Cleaned_Restaurant_Inspections_No_Dupes.geojson',
+               driver='GeoJSON')
+
+
+
 def main():
-    clean_restaurant_data()
+    # clean_restaurant_data()
+    get_rid_duplicates()
 
 
 if __name__ == '__main__':
